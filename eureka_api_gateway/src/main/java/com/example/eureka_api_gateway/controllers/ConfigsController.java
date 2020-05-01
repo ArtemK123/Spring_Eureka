e958b056386a184b86688c469680beb478da6709
unknown <darkmode1012@gmail.com>
2020-05-01 19:20:39 +0300

    Used @value and configuration class in gateway and client /configs controller

--- eureka_api_gateway/src/main/java/com/example/eureka_api_gateway/controllers/ConfigsController.java
+++ eureka_api_gateway/src/main/java/com/example/eureka_api_gateway/controllers/ConfigsController.java
@@ -3,33 +3,35 @@ package com.example.eureka_api_gateway.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.example.eureka_common.providers.ApplicationPropertiesProvider;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigsController {
-    private final String FIRST_PROPERTY_NAME = "custom.test.gatewayProperty1";
-    private final String SECOND_PROPERTY_NAME = "custom.test.gatewayProperty2";
-    private final String THIRD_PROPERTY_NAME = "custom.test.gatewayProperty3";
+    private static final String FIRST_PROPERTY_NAME = "custom.test.gatewayProperty1";
+    private static final String SECOND_PROPERTY_NAME = "custom.test.gatewayProperty2";
+    private static final String THIRD_PROPERTY_NAME = "custom.test.gatewayProperty3";
     
-    private ApplicationPropertiesProvider applicationPropertiesProvider;
+    @Value("${" + FIRST_PROPERTY_NAME + "}")
+    private String firstPropertyValue;
     
-    @Autowired
-    public ConfigsController(ApplicationPropertiesProvider applicationPropertiesProvider) {
-        this.applicationPropertiesProvider = applicationPropertiesProvider;
-    }
- 
+    @Value("${" + SECOND_PROPERTY_NAME + "}")
+    private String secondPropertyValue;
+    
+    @Value("${" + THIRD_PROPERTY_NAME + "}")
+    private String thirdPropertyValue;
+
     @RequestMapping(value = "/configs", method = RequestMethod.GET)
     public ResponseEntity<Map<String, String>> configs() {
         Map<String, String> response = new HashMap<String, String>();
-        response.put(FIRST_PROPERTY_NAME, applicationPropertiesProvider.get(FIRST_PROPERTY_NAME));
-        response.put(SECOND_PROPERTY_NAME, applicationPropertiesProvider.get(SECOND_PROPERTY_NAME));
-        response.put(THIRD_PROPERTY_NAME, applicationPropertiesProvider.get(THIRD_PROPERTY_NAME));
+        response.put(FIRST_PROPERTY_NAME, firstPropertyValue);
+        response.put(SECOND_PROPERTY_NAME, secondPropertyValue);
+        response.put(THIRD_PROPERTY_NAME, thirdPropertyValue);
 
         return ResponseEntity.ok(response);
     }
--- eureka_client/src/main/java/com/example/eureka_client/controllers/ConfigsController.java
+++ eureka_client/src/main/java/com/example/eureka_client/controllers/ConfigsController.java
@@ -3,7 +3,7 @@ package com.example.eureka_client.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.example.eureka_common.providers.ApplicationPropertiesProvider;
+import com.example.eureka_client.config.CustomTestConfigs;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,19 +17,19 @@ public class ConfigsController {
     private final String SECOND_PROPERTY_NAME = "custom.test.clientProperty2";
     private final String THIRD_PROPERTY_NAME = "custom.test.clientProperty3";
     
-    private ApplicationPropertiesProvider applicationPropertiesProvider;
+    private CustomTestConfigs customTestConfigs;
     
     @Autowired
-    public ConfigsController(ApplicationPropertiesProvider applicationPropertiesProvider) {
-        this.applicationPropertiesProvider = applicationPropertiesProvider;
+    public ConfigsController(CustomTestConfigs customTestConfigs) {
+        this.customTestConfigs = customTestConfigs;
     }
  
     @RequestMapping(value = "/configs", method = RequestMethod.GET)
     public ResponseEntity<Map<String, String>> configs() {
         Map<String, String> response = new HashMap<String, String>();
-        response.put(FIRST_PROPERTY_NAME, applicationPropertiesProvider.get(FIRST_PROPERTY_NAME));
-        response.put(SECOND_PROPERTY_NAME, applicationPropertiesProvider.get(SECOND_PROPERTY_NAME));
-        response.put(THIRD_PROPERTY_NAME, applicationPropertiesProvider.get(THIRD_PROPERTY_NAME));
+        response.put(FIRST_PROPERTY_NAME, customTestConfigs.getClientProperty1());
+        response.put(SECOND_PROPERTY_NAME, customTestConfigs.getClientProperty2());
+        response.put(THIRD_PROPERTY_NAME, customTestConfigs.getClientProperty3());
 
         return ResponseEntity.ok(response);
     }
