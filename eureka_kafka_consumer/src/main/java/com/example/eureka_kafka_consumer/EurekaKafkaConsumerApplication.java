6d284670b07e5b8296e4ad85f2ab20d0482cc52d
unknown <darkmode1012@gmail.com>
2020-05-19 22:10:50 +0300

    Added kafka-consumer project

---
+++ eureka_kafka_consumer/src/main/java/com/example/eureka_kafka_consumer/EurekaKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.eureka_kafka_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaKafkaConsumerApplication.class, args);
+	}
+
+}
