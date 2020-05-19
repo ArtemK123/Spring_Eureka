924bc45892fc19fc831e0831c812cd864f8e4a21
unknown <darkmode1012@gmail.com>
2020-05-19 22:37:18 +0300

    Configured kafka-consumer to work with eureka-server and config server

--- eureka_kafka_consumer/src/main/java/com/example/eureka_kafka_consumer/EurekaKafkaConsumerApplication.java
+++ eureka_kafka_consumer/src/main/java/com/example/eureka_kafka_consumer/EurekaKafkaConsumerApplication.java
@@ -2,8 +2,10 @@ package com.example.eureka_kafka_consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EurekaKafkaConsumerApplication {
 
 	public static void main(String[] args) {
